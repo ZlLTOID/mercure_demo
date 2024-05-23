c203d53bebe9a886e552c23fc25f9ffcf40dbf73
JanDolezal <dolezal6@gmail.com>
2024-05-23 18:49:43 +0200

    edit run.sh

--- run.sh
+++ run.sh
@@ -2,12 +2,12 @@ cd backend
 composer install --no-interaction
 
 cd ..
-cd frontend
+cd web
 npm install
 
 wget https://github.com/dunglas/mercure/releases/download/v0.15.11/mercure_Linux_x86_64.tar.gz
 mkdir /opt/mercure-copied
 tar -xf mercure_Linux_x86_64.tar.gz -C /opt/mercure-copied/
 rm mercure_Linux_x86_64.tar.gz
-cd /opt/mercure
+cd /opt/mercure-copied
 MERCURE_PUBLISHER_JWT_KEY='!ChangeThisMercureHubJWTSecretKey!' MERCURE_SUBSCRIBER_JWT_KEY='!ChangeThisMercureHubJWTSecretKey!' ./mercure run --config Caddyfile.dev
